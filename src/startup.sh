3441ae360fd1b8c909537e59e596c91cc08b3ff1
GH0STAV0 <you@example.com>
2021-12-24 01:48:44 +0800

    add new project

--- src/startup.sh
+++ src/startup.sh
@@ -75,7 +75,7 @@ systemctl start supervisor
 # cd 48_firefox/
 # bash ./l1.sh &
 echo "lol" > /root/go
-host_name = ${hostname -1}
+host_name = ${hostname: -1}
 echo $host_name
 ###########################################
 # /usr/bin/supervisord -n
