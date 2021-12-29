7c987b263ae0e1abf15a74abf8fb0aaaddc8f6f6
GH0STAV0 <you@example.com>
2021-12-29 16:08:45 -0700

    add new project

--- src/go_go.sh
+++ src/go_go.sh
@@ -2,6 +2,10 @@
 clear
 trap "echo oh;exit" SIGTERM SIGINT
 echo -e "nameserver 8.8.8.8\nnameserver 8.8.4.4" >  /etc/resolv.conf
+cd /root/SDA_ALL/
+rm -rf site_5
+git reset --hard
+git pull
 cd /root/SDA_ALL/site_5/
 chmod +x *
 pwd
@@ -9,5 +13,5 @@ while true
 do
 	echo "NEW ..............."
 	cd /root/SDA_ALL/site_5/
-	timeout 3m python3 48_ads.py
+	timeout 5m python3 48_ads.py
 done
